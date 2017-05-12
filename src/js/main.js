0ce6b30ceda1d07d759a5aa6104e61bbe8f5d584
Brigitem <brigitem55@gmail.com>
2017-05-12 11:29:46 +0200

    Sync all changes

--- src/js/main.js
+++ src/js/main.js
@@ -21,24 +21,41 @@ $(window).resize(function() {
 });
 
 $(window).scroll(function() {
-	var scrollAmount = $(window).scrollTop();
-	if (scrollAmount > headerHeight) {
-		$('nav').css({
-			'position': 'fixed'
-		});
-	} else {
-		$('nav').css({
-			'position': 'absolute'
-		});
-	}
-	var scrollTotal = scrollAmount + $(window).height();
-	if (scrollTotal > documentHeight) {
-		$('.scroll-to-top').css({
-			'display': 'block'
-		});
-	} else {
-		$('.scroll-to-top').css({
-			'display': 'none'
-		});
-	}
+	// var scrollAmount = $(window).scrollTop();
+	// if (scrollAmount > headerHeight) {
+	// 	$('nav').css({
+	// 		'position': 'fixed'
+	// 	});
+	// } else {
+	// 	$('nav').css({
+	// 		'position': 'absolute'
+	// 	});
+	// }
+	// var scrollTotal = scrollAmount + $(window).height();
+	// if (scrollTotal > documentHeight) {
+	// 	$('.scroll-to-top').css({
+	// 		'display': 'block'
+	// 	});
+	// } else {
+	// 	$('.scroll-to-top').css({
+	// 		'display': 'none'
+	// 	});
+	// }
 });
+
+
+$( "section.grid div.grid-item li" ).hover(hoverIn, hoverOut);
+
+
+function hoverIn(){
+	var currentLiItem = $(this).attr('id');
+	var hoverBoxClass = ".hover."+currentLiItem;
+	$(hoverBoxClass).addClass('active');
+}
+
+
+function hoverOut(){
+	var currentLiItem = $(this).attr('id');
+	var hoverBoxClass = ".hover."+currentLiItem;
+	$(hoverBoxClass).removeClass('active');
+}
